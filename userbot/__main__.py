3236ac940dc8d9782997caf74e3268c9e9825b82
Kisal Kawshika <77261949+KisalKawshika-ctrl@users.noreply.github.com>
2021-02-24 20:12:25 +0530

    Add files via upload

---
+++ userbot/__main__.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot start point """
+
+from importlib import import_module
+from sys import argv
+
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+from userbot import LOGS, BOT_VERSION, bot
+from userbot.modules import ALL_MODULES
+
+
+INVALID_PH = '\nERROR: The phone no. entered is INVALID!' \
+             '\n  Tip: Use country code (eg +62) along with num.' \
+             '\n       Recheck your phone number and try again!'
+
+try:
+    bot.start()
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    exit(1)
+
+for module_name in ALL_MODULES:
+    imported_module = import_module("userbot.modules." + module_name)
+
+LOGS.info(f"You are running Alpha UserBot v{BOT_VERSION}")
+LOGS.info("Test it by typing .alive on any chat.")
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
